e8d28096e7771de091f4dfb8dccc0316c155f4e2
Leonardo Bonetti <Leonardo.bonetti@etec.sp.gov.br>
2019-10-30 20:58:18 -0300

    first commit

---
+++ cdc-admin/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Leo\Documents\Projetos\Aula React\cdc-admin
+npm start
---
+++ cdc-admin/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+import {Link} from 'react-router-dom';
+
+
+const Header = () => (
+
+    <a href="/"><header id="main-header">Hello</header></a>
+
+);
+
+export default Header
